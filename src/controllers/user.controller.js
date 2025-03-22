b7c0fd6b295b611553694e44f77702b18633827a
dhiraj-001 <gogoidhiraj207@gmail.com>
2025-03-22 12:19:10 +0530

    add register controller

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,10 +1,64 @@
+import { User } from "../models/user.model.js";
+import ApiError from "../utils/ApiError.js";
 import { asyncHandeler } from "../utils/asyncHandeler.js";
+import { uploadOnCloudinary } from "../utils/cloudinary.js";
+import {ApiResponse } from "../utils/ApiResponse.js";
+const registerUser = asyncHandeler(async (req, res) => {
+  // get user details from frontend
+  // validation
+  // check for duplicate user
+  // check from image, avater
+  // upload in cloudinary
+  // create user object - create entry in db
+  // remove password and refresh token from response
+  // check for user creation
+  // return response
 
-const registerUser = asyncHandeler (async (req, res) =>{
- return res.status(200).json({
-    message: "ok"
+  const { userName, email, fullName, password } = res.body;
+
+  if ([userName, email, fullName, password].some((val) => val?.trim() === "")) {
+    throw new ApiError("400", "All fields are required");
+  }
+
+  const existedUser = await User.findOne({
+    $or: [{ email }, { userName }],
+  });
+  if (existedUser) {
+    throw new ApiError(409, "User with email or username already exists");
+  }
+
+  const avatarLocalPath = req.files?.avatar[0]?.path;
+  const coverImagePath = req.files?.coverImage[0]?.path;
+  if (!avatarLocalPath) {
+    throw new ApiError(400, "Avatar file is missing");
+  }
+
+  const avatar = await uploadOnCloudinary(avatarLocalPath);
+  const coverImage = await uploadOnCloudinary(coverImagePath);
+  if (!avatar) {
+    throw new ApiError(400, "Avatar file is missing");
+  }
+
+  const user = await User.create({
+    userName: userName.toLowerCase(),
+    email,
+    fullName,
+    password,
+    avatar : avatar.url,
+    coverImage : coverImage.url || "",
   })
-})
 
+  const createdUser = await User.findById(user._id).select(
+    "-password -refreshToken"
+  )
+  if (!createdUser) {
+    throw new ApiError(500, "Failed to create user");
+  }
 
-export {registerUser} 
+  return res.status(201).json(
+    new ApiResponse(200, createdUser, "User created successfully")
+  )
+
+});
+
+export { registerUser };
--- src/utils/ApiResponse.js
+++ src/utils/ApiResponse.js
@@ -1,8 +1,11 @@
 class ApiResponse{
-  constructor(statusCode, message = "Success", data) {
+  constructor(statusCode,data, message = "Success" ) {
     this.statusCode = statusCode;
     this.message = message;
     this.data = data;
     this.success = statusCode < 400; // statuscode below 400 is used for response from api or success ... For errors use statusCode above 400
   }
 }
+
+
+export {ApiResponse}
