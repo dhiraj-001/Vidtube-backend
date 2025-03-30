f6237ca1899b09bc7190487aac639af631cdb48b
dhiraj-001 <gogoidhiraj207@gmail.com>
2025-03-31 00:08:37 +0530

    subsrciption pipeline mongodb

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -382,9 +382,42 @@ const getUserChannelProfile = asyncHandeler(async (req, res) => {
       $addFields: {
         subscribersCount: { $size: "$subscribers" },
         subscriberedToCount: { $size: "$subscriberedTo" },
+        isSubscribed:{
+          $cond:{
+            if:{
+              $in: [req.user._id, "$subscribers.subscriber"]
+            },
+            then: true,
+            else: false
+          }
+        }
       },
     },
+    {
+      $project: {
+        _id: 1,
+        userName: 1,
+        email: 1,
+        fullName: 1,
+        avatar: 1,
+        coverImage: 1,
+        subscribersCount: 1,
+        subscriberedToCount: 1,
+        isSubscribed: 1,
+      },
+    }
   ]);
+  console.log(channel);
+
+  if(channel?.length){
+    throw new ApiError(404, "Channel doesnot exist");
+  }
+
+  return res
+  .status(200)
+  .json(new ApiResponse(
+    200, channel[0], "Channel profile retrieved successfully"
+  ));
 });
 
 export {
